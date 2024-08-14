ca88e8b25ab8e10e90c2b9bbc2ce33ac6f057401
Maty <r.m.barrientos@live.com>
2024-08-14 20:27:46 -0300

    subida de workspace a mi repo de GitHub

---
+++ workspace/Clase3/pruebitaMatrices.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+void cargarMatriz(int m[3][5], int fila, int columna)
+{
+
+    int i,j;
+    for(i=0; i<fila; i++)
+    {
+        for(j=0; j<columna; j++)
+        {
+            cout<<"Ingresa un número: ";
+            cin>>m[i][j];
+        }
+    }
+}
+
+void mostrarMatriz(int m[3][5], int fila, int columna)
+{
+    int i,j;
+    for(i=0; i<fila; i++)
+    {
+        for(j=0; j<columna; j++)
+        {
+            cout<<m[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    int mat[3][5];
+    cargarMatriz(mat,3,5);
+    mostrarMatriz(mat,3,5);
+
+    return 0;
+}
